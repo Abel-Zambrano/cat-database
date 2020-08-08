b2fc7fb7ce81aa55bd78b85df974220a08c2784e
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-08-08 09:48:28 -0700

    updated database

--- app.js
+++ app.js
@@ -11,24 +11,25 @@ app.set('view engine', 'ejs');
 const catSchema = new mongoose.Schema({
     id: String,
     url: String,
-    votes: Number
+    vote: Number
         
 });
 
 //Compile Model
 const Cat = mongoose.model('Cat', catSchema);
 
+//add cats to database
 // Cat.create(
-//     { id:1, url:'https://cdn2.thecatapi.com/images/a9j.jpg'},
-//     { id:3, url:'https://cdn2.thecatapi.com/images/bir.jpg'},
-//     { id:4, url:'https://cdn2.thecatapi.com/images/9jf.jpg'},
-//     { id:5, url:'https://cdn2.thecatapi.com/images/bkc.jpg'},
-//     { id:6, url:'https://cdn2.thecatapi.com/images/d2o.jpg'},
-//     { id:7, url:'https://cdn2.thecatapi.com/images/MTk2NTM4NA.jpg'},
-//     { id:8, url:'https://cdn2.thecatapi.com/images/ajA3tUqUR.jpg'},
-//     { id:9, url:'https://cdn2.thecatapi.com/images/7l5.jpg'},
-//     { id:10, url:'https://cdn2.thecatapi.com/images/e9d.jpg'},
-//     { id:2, url:'https://cdn2.thecatapi.com/images/dje.jpg'},
+//     { id:1, url:'https://cdn2.thecatapi.com/images/a9j.jpg', vote:0},
+//     { id:3, url:'https://cdn2.thecatapi.com/images/bir.jpg', vote:0},
+//     { id:4, url:'https://cdn2.thecatapi.com/images/9jf.jpg', vote:0},
+//     { id:5, url:'https://cdn2.thecatapi.com/images/bkc.jpg', vote:0},
+//     { id:6, url:'https://cdn2.thecatapi.com/images/d2o.jpg', vote:0},
+//     { id:7, url:'https://cdn2.thecatapi.com/images/MTk2NTM4NA.jpg', vote:0},
+//     { id:8, url:'https://cdn2.thecatapi.com/images/ajA3tUqUR.jpg', vote:0},
+//     { id:9, url:'https://cdn2.thecatapi.com/images/7l5.jpg', vote:0},
+//     { id:10, url:'https://cdn2.thecatapi.com/images/e9d.jpg', vote:0},
+//     { id:2, url:'https://cdn2.thecatapi.com/images/dje.jpg', vote:0},
 //     (err, cat) => {
 //         if(err) {
 //             console.log(err);
@@ -47,6 +48,8 @@ app.get('/', (req, res) => {
             console.log(err);
 
         } else {
+            const upVote = allCats.filter(cats => cats.votes === 1);
+            const downVote = allCats.filter(cats => cats.votes === 0)
             const randomCat = allCats[Math.floor(Math.random() * allCats.length)];
             res.render('index', {cats: randomCat});
             
@@ -56,7 +59,7 @@ app.get('/', (req, res) => {
 
 });
 
-app.post('/', (req, res) => {
+app.put('/', (req, res) => {
     Cat.find({}, (err, allCats) => {
         if(err) {
             console.log(err);
