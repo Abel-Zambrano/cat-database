0258725209c7e941024f2f7a7596a139184904dd
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-08-09 21:34:59 -0700

    update

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ app.put('/downvote/:id', (req, res) => {
     })
 })
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server is running on PORT:3000');
     
 })
